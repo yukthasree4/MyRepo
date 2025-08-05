b61df11e212843722d27b039e3594b37239483b0
root <root@ip-172-31-20-19.eu-north-1.compute.internal>
2025-08-05 10:46:22 +0000

    committing 3files

---
+++ file1.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // This line prints "Hello, World!" to the console
+        System.out.println("Hello, World!"); 
+    }
+}
